445e5b51a06ae9892241321c5782a6d31d500312
Jaco van Schalkwyk <jaco.vanschalkwyk@entelect.co.za>
2023-07-03 15:53:11 +0200

    done with phase 1

---
+++ Phase 1/src/01. StartSolution/Core.Contracts/Usings.cs
@@ -0,0 +1,3 @@
+﻿global using Common.Mappers;
+global using Common.Models;
+global using Core.Contracts;
---
+++ Phase 1/src/01. StartSolution/Core/DependancyInjection.cs
@@ -0,0 +1,14 @@
+﻿namespace Core;
+
+using Core.Contracts;
+using Microsoft.Extensions.DependencyInjection;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddTransient(typeof(IPizzaCore), typeof(PizzaCore));
+
+        return services;
+    }
+}
